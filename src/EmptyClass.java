b6c5fbf609c6dfaa6afcba56cf671b47f0781a50
oguzhanasikkaya <oguzhanasikkaya@yahoo.com>
2022-07-23 12:06:59 +0300

    hello

--- src/EmptyClass.java
+++ src/EmptyClass.java
@@ -3,6 +3,7 @@ public class EmptyClass {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("how are you");
+        System.out.println("I am fine");
     }
 
 }
